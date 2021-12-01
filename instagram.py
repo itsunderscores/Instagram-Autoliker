0d7b5a04280fd69999d79c5c5628e33619be1b3a
Underscores <84211119+itsunderscores@users.noreply.github.com>
2021-11-30 18:20:09 -0600

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -51,7 +51,7 @@ def header():
 	print(WHITE+"-------------------------------------------------------"+YELLOW)
 
 firefoxOptions = Options()
-#firefoxOptions.add_argument("-headless")
+firefoxOptions.add_argument("-headless")
 #firefoxOptions.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0")
 browser = webdriver.Firefox(executable_path="C:\\xampp\\htdocs\\geckodriver.exe", options=firefoxOptions)
 
